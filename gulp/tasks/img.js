cbbda497263d2b781baf82635a8ef6580f6f46e5
Dementr-Slash <dmi56756877@yandex.ru>
2018-05-11 01:18:01 +0900

    change logo

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -4,14 +4,20 @@ module.exports = function() {
             .pipe($.gulp.dest('./build/static/img/'));
     });
 
+    // $.gulp.task('img:build', () => {
+    //     return $.gulp.src('./dev/static/img/**/*.{png,jpg,gif}')
+	// 		.pipe($.gp.imagemin({
+	// 			optimizationLevel: 5, 
+	// 			progressive: true, 
+	// 			svgoPlugins: [{removeViewBox: false}], 
+	// 			interlaced: true
+	// 		}))
+    //         .pipe($.gulp.dest('./build/static/img/'));
+    // });
+    
     $.gulp.task('img:build', () => {
         return $.gulp.src('./dev/static/img/**/*.{png,jpg,gif}')
-			.pipe($.gp.imagemin({
-				optimizationLevel: 5, 
-				progressive: true, 
-				svgoPlugins: [{removeViewBox: false}], 
-				interlaced: true
-			}))
+            .pipe($.gp.tinypng('ROs7wvgvqNPWKO1oKB70pi2cZByPlN6t'))
             .pipe($.gulp.dest('./build/static/img/'));
     });
 
@@ -21,3 +27,4 @@ module.exports = function() {
             .pipe($.gulp.dest('./build/static/img/general/'));
     });
 };
+;
